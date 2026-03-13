70f6fa99138f722625c3225c5adebfac299f6107
CROWNguyenVietDung <nguyendungsenpai06@gmail.com>
2026-03-13 16:46:36 +0700

    base

---
+++ src/main/java/com/winistore/win/WinIStoreApplication.java
@@ -0,0 +1,13 @@
+package com.winistore.win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinIStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinIStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winistore/win/WinIStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winistore.win;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinIStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
